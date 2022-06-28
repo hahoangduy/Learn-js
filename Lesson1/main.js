c167780a2d3dbb876d77094b991b8b594f66950a
hahoangduy <hahoangduy268@gmail.com>
2022-06-28 21:33:40 +0700

    hoc them code js

--- Lesson1/main.js
+++ Lesson1/main.js
@@ -228,3 +228,24 @@ Kiểu dữ liệu trong Javascript
         } else {
             console.log('A > B');
         }
+
+    /*
+    Hàm (Function) trong Javascript
+
+        1. Hàm ?
+            - Một khối mãi
+            - Làm 1 việc cụ thể
+        2. Loại hàm
+            - Built-in
+            - Tự định nghĩa 
+        3. Tính chất
+            - Không thực thi khi định nghĩa
+            - Sẽ thực thi khi được gọi
+            - Có thể nhận tham số
+            - Có thể trả về 1 giá trị
+        4. Tạo hàm đầu tiên 
+    */
+        function showDialog() {
+            console.log('hello cac ban');
+        }
+            showDialog();
