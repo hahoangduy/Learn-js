f57ea2a8418b2ec094310ab62a48a5c75b96d155
hahoangduy <hahoangduy268@gmail.com>
2022-07-07 14:30:56 +0700

    them folder moi

---
+++ BaiTap2/main.js
@@ -0,0 +1,16 @@
+// cho 1 function find() có 2 tham số, array và index, 
+// nếu index và giá trị của index là số chẵn thì tính tổng giá trị trong array,
+// nếu không thì trả về tổng giữa index và độ dài của array.
+
+
+// arr1 = [4, 7,22,65,87,34, 9]
+// find(arr1, 2)
+
+// find(arr1, 5)
+
+
+function find(array, index) {
+    for(let i = 0;  < array.length; i++) {
+
+    }
+}
