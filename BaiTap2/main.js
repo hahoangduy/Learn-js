f958d4d8d3f9eccc79d6f9e79504a0dc21cec241
hahoangduy <hahoangduy268@gmail.com>
2022-07-07 22:05:17 +0700

    lam bai tap

--- BaiTap2/main.js
+++ BaiTap2/main.js
@@ -3,37 +3,23 @@
 // vd: tháng 7 là 31 ngày. nếu k có tháng tồn tại trả về  -1
 
 function find(thang) {
-    if(thang == 1) {
-        return '31 ngày'
-    } else if(thang == 2) {
-        return '30 ngày'
-    } else if(thang == 3) {
-        return '31 ngày'
-    } else if(thang == 4) {
-        return '30 ngày'
-    } else if(thang == 5) {
-        return '31 ngày'
-    } else if(thang == 6) {
-        return '30 ngày'
-    } else if(thang == 7) {
-        return '31 ngày'
-    } else if(thang == 8) {
-        return '31 ngày'
-    } else if(thang == 9) {
-        return '30 ngày'
-    } else if(thang == 10) {
-        return '31 ngày'
-    } else if(thang == 11) {
-        return '30 ngày'
-    } else if(thang == 12) {
-        return '31 ngày'
-    } else {
-        return -1
+        if(thang == 1 || thang == 3 || thang ==  5 || thang == 7 || thang == 8 || thang == 10 || thang == 12) {
+            return '31 ngày'
+        } else if(thang == 2) {
+            return '28 ngày'
+        } else if(thang == 4 || thang == 6 || thang == 9 || thang == 11) {
+            return '30 ngày'
+        } else {
+            return -1
+        }
     }
-}
 
-var result = find(8)
+var result = find(9)
 console.log(result)
+var result1 = find(2)
+console.log(result1)
+var result2 = find(7)
+console.log(result2)
 
 
 
@@ -43,4 +29,7 @@ console.log(result)
 // – 600 đồng cho mỗi gọi của 50 phút đầu tiên.
 // – 400 đồng cho mỗi phút gọi của 150 phút tiếp theo.
 // – 200 đồng cho bất kỳ mỗi phút nào sau 200 phút đầu tiên.
+
+// số phút 100 -> output = 75000 (30000 + 20000)
+// số phút 250 -> output = 125000 (30000 + 60000 + 10000)
 
