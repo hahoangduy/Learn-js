26b78b6657a9e163d9c406a68a36f6c5f63a86d8
hahoangduy <hahoangduy268@gmail.com>
2022-07-07 16:15:09 +0700

    on lai code

--- BaiTap2/main.js
+++ BaiTap2/main.js
@@ -10,7 +10,43 @@
 
 
 function find(array, index) {
-    for(let i = 0;  < array.length; i++) {
-
+    var sum = 0
+    // Tạo if để xác định index và giá trị của index là số chẵn hay không
+        if(index % 2 == 0 && array[index] % 2 == 0) { // index và giá trị ứng với index đó là số chẵn
+            array.forEach(function(value){ // tính tổng giá trị trong array
+                sum = sum + value;
+            }) 
+        } else { // index và giá trị ứng với index đó là số lẻ
+            sum = index + array.length // tính tổng giữa index và độ dài của array
+        }
+        return sum
     }
+
+
+arr1 = [4, 7, 22, 65, 87, 34, 9]
+var result = find(arr1, 2)
+console.log(result)
+var result1 = find(arr1, 5)
+console.log(result1)
+
+console.log('---------------------------------------------------------------------')
+
+function find1(array, index) {
+    var sum = 0
+    // Tạo if để xác định index và giá trị của index là số chẵn hay không
+    if(index % 2 == 0 && array[index] % 2 == 0) {
+    for(let i = 0; i < array.length; i++) {
+            sum = sum + array[i]
+    }
+    } else {
+        sum = index + array.length
+    }
+    return sum
 }
+
+
+arr1 = [4, 7, 22, 65, 87, 34, 9]
+var result2 = find1(arr1, 2)
+console.log(result2)
+var result3 = find1(arr1, 5)
+console.log(result3)
